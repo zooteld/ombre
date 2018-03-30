1c49b3b72c88ecb7440fb892e442339222cac853
SadBatman <niels.heinen@gmail.com>
2018-03-30 17:23:58 +0200

    Check the version of a block when adding an alternative block (and small bug fix)

--- src/cryptonote_core/hardfork.h
+++ src/cryptonote_core/hardfork.h
@@ -1,22 +1,22 @@
 // Copyright (c) 2014-2017, The Monero Project
 // Copyright (c) 2017, SUMOKOIN
-// 
+//
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without modification, are
 // permitted provided that the following conditions are met:
-// 
+//
 // 1. Redistributions of source code must retain the above copyright notice, this list of
 //    conditions and the following disclaimer.
-// 
+//
 // 2. Redistributions in binary form must reproduce the above copyright notice, this list
 //    of conditions and the following disclaimer in the documentation and/or other
 //    materials provided with the distribution.
-// 
+//
 // 3. Neither the name of the copyright holder nor the names of its contributors may be
 //    used to endorse or promote products derived from this software without specific
 //    prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
 // EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 // MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
@@ -113,6 +113,20 @@ namespace cryptonote
      * hardfork object to reorganize.
      */
     bool check(const cryptonote::block &block) const;
+
+    /**
+     * @brief same as check, but for a particular height, rather than the top
+     *
+     * NOTE: this does not play well with voting, and relies on voting to be
+     * disabled (that is, forks happen on the scheduled date, whether or not
+     * enough blocks have voted for the fork).
+     *
+     * returns true if no error, false otherwise
+     *
+     * @param block the new block
+     * @param height which height to check for
+     */
+    bool check_for_height(const cryptonote::block &block, uint64_t height) const;
 
     /**
      * @brief add a new block
@@ -212,6 +226,8 @@ namespace cryptonote
 
     uint8_t get_block_version(uint64_t height) const;
     bool do_check(uint8_t block_version, uint8_t voting_version) const;
+    bool do_check_for_height(uint8_t block_version, uint8_t voting_version, uint64_t height) const;
+
     int get_voted_fork_index(uint64_t height) const;
     uint8_t get_effective_version(uint8_t voting_version) const;
     bool add(uint8_t block_version, uint8_t voting_version, uint64_t height);
