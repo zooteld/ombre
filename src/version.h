98661f2cd8ccc91e44b0181aadc3691ed577f3d8
Ahmad Yoosofan <yoosofan@myfastmail.com>
2019-07-06 00:25:58 +0430

    start to rebase to Ryo

--- src/version.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-extern const char* const OMBRE_VERSION_TAG;
-extern const char* const OMBRE_VERSION;
-extern const char* const OMBRE_RELEASE_NAME;
-extern const char* const OMBRE_VERSION_FULL;
